547bccc944cc5e8287bbf896e22842ab440fe256
vicfw <faridforward2223@gmail.com>
2021-09-09 12:18:43 +0430

    implent body parser limit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const limiter = rateLimit({
 app.use("/api", limiter);
 app.use(helmet());
 
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(express.static("public"));
 
 app.use(cors({ credentials: true, origin: true }));
