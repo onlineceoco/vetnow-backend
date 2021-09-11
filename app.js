8a58cac4b067b16235b3ffe74564fbb9cb50c1dd
vicfw <faridforward2223@gmail.com>
2021-09-11 11:51:27 +0430

    security done

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const globalErrorHandler = require("./controller/error.controller");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const app = express();
 app.use(cors({ credentials: true, origin: true }));
+app.use(express.static("public"));
 
 const limiter = rateLimit({
   max: 100,
@@ -17,7 +21,13 @@ app.use("/api", limiter);
 app.use(helmet());
 
 app.use(express.json({ limit: "10kb" }));
-app.use(express.static("public"));
+
+//no-sql injection after body parser
+app.use(mongoSanitize());
+//prevent xss attack after body parser
+app.use(xss());
+//prevent parameter pollution
+// app.use(hpp({ whitelist: ["category"] }));
 
 app.use("/api/v1/users", require("./routes/users"));
 app.use("/api/v1/products", require("./routes/product"));
