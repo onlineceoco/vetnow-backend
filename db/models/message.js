a27717a50b023b2dcdc360cd3af75a4a8bf6d846
vicfw <faridforward2223@gmail.com>
2021-10-09 17:03:12 +0330

    doctor chat massaeges now save in db

--- db/models/message.js
+++ db/models/message.js
@@ -1,20 +1,28 @@
 const mongoose = require("mongoose");
 
 const chatMessageSchema = new mongoose.Schema({
-  chatroom: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: [true, "نام چت روم نمیتواند خالی بماند"],
-    ref: "Chatroom",
-  },
+  // chatroom: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   required: [true, "نام چت روم نمیتواند خالی بماند"],
+  //   ref: "Chatroom",
+  // },
   user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    type: Object,
     required: true,
   },
-  message: {
-    type: String,
-    required: [true, "پیام نمیتواند خالی بماند"],
-  },
+  message: [
+    {
+      text: {
+        type: String,
+        required: true,
+      },
+
+      user: {
+        type: Object,
+        required: true,
+      },
+    },
+  ],
 });
 
 module.exports = mongoose.model("Message", chatMessageSchema);
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const User = require("./db/models/User.js");
 const Message = require("./db/models/message");
 const redis = require("redis");
-// const RedisClient = redis.createClient();
+const RedisClient = redis.createClient();
 const SocketIOFileUpload = require("socketio-file-upload");
 
 process.on("uncaughtException", err => {
@@ -72,26 +72,37 @@ io.use(async (socket, next) => {
 
 io.on("connection", socket => {
   console.log("joiend");
-  socket.on("joinroom", roomId => {
+  socket.on("joinroom", async roomId => {
+    if (socket.user.role === "user") {
+      const isChatExist = await Message.findOne({ user: socket.user._id });
+      isChatExist && socket.emit("send_exist_messages_from_db", isChatExist);
+    }
+
     socket.join(roomId);
   });
   socket.on("chatroomMessage", async ({ message, roomId }, callback) => {
-    console.log(message);
     if (message) {
-      // await Message.create({
-      //   chatroom: roomId,
-      //   user: socket.user._id,
-      //   message,
-      // });
       io.to(roomId).emit("newMessage", {
         message: {
-          text: message.message,
-          user: socket.user._id,
+          text: message,
+          user: socket.user,
         },
-        name: socket.user.name ? socket.user.name : socket.user.phone,
       });
     }
     callback();
+  });
+
+  socket.on("save_to_db", async messages => {
+    if (socket.user.role === "user" && messages) {
+      const isUserChatExist = await Message.findOneAndUpdate(
+        { user: socket.user._id },
+        { $set: { message: messages } },
+        { new: true },
+      );
+      if (!isUserChatExist) {
+        await Message.create({ user: socket.user._id, message: messages });
+      }
+    }
   });
 
   socket.on("leaveroom", roomId => {
@@ -104,123 +115,126 @@ io.on("connection", socket => {
 
 //support chat
 
-// const supportNameSpace = io.of("/support");
-// supportNameSpace.use(async (socket, next) => {
-//   if (socket.handshake.auth && socket.handshake.auth.token) {
-//     const token = socket.handshake.auth.token;
-//     const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-//     // 3) Check if user still exists
-//     const currentUser = await User.findOne({ phone: decoded.id });
-//     if (!currentUser) {
-//       return next(
-//         new AppError(
-//           "The user belonging to this token does no longer exist.",
-//           401,
-//         ),
-//       );
-//     }
-//     socket.user = currentUser;
-//     next();
-//   } else {
-//     next();
-//   }
-// });
-// io.of("/support").on("connection", async socket => {
-//   //file upload
-//   const uploader = new SocketIOFileUpload();
-//   uploader.dir = "./public/support";
-//   uploader.listen(socket);
+const supportNameSpace = io.of("/support");
+supportNameSpace.use(async (socket, next) => {
+  if (socket.handshake.auth && socket.handshake.auth.token) {
+    const token = socket.handshake.auth.token;
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    // 3) Check if user still exists
+    const currentUser = await User.findOne({ phone: decoded.id });
+    if (!currentUser) {
+      return next(
+        new AppError(
+          "The user belonging to this token does no longer exist.",
+          401,
+        ),
+      );
+    }
+    socket.user = currentUser;
+    next();
+  } else {
+    next();
+  }
+});
 
-//   //support room
-//   if (socket.user && socket.user.role === "oprator") {
-//     //support chat
-//     socket.join("supportRoom");
-//     //finding supports that Connected
-//     const sockets = await supportNameSpace.in("supportRoom").fetchSockets();
-//     let connectedSupports = [];
-//     for (const socket of sockets) {
-//       connectedSupports.push(socket.user);
-//     }
-//     supportNameSpace.emit("current_supports", connectedSupports);
+//support chat
 
-//     socket.on("support_msg", (msg, room, callback) => {
-//       // new client message notif to supports
-//       supportNameSpace.to(room).emit("new_msg", { msg, room });
-//       callback();
-//     });
-//     //get client first msg
-//     socket.on("loaded", room => {
-//       // socket.join(room);
-//       const stringRoom = JSON.stringify(room);
+io.of("/support").on("connection", async socket => {
+  //file upload
+  const uploader = new SocketIOFileUpload();
+  uploader.dir = "./public/support";
+  uploader.listen(socket);
 
-//       RedisClient.hgetall(stringRoom, function (err, value) {
-//         if (value && value.msg)
-//           supportNameSpace.to(room).emit("client_first_msg_server", value.msg);
-//       });
-//     });
+  //support room
+  if (socket.user && socket.user.role === "oprator") {
+    //support chat
+    socket.join("supportRoom");
+    //finding supports that Connected
+    const sockets = await supportNameSpace.in("supportRoom").fetchSockets();
+    let connectedSupports = [];
+    for (const socket of sockets) {
+      connectedSupports.push(socket.user);
+    }
+    supportNameSpace.emit("current_supports", connectedSupports);
 
-//     //on disconnect
-//     socket.on("disconnect", () => {
-//       const index = connectedSupports.findIndex(
-//         support => support._id === socket.user._id,
-//       );
-//       const remianSupports = connectedSupports.splice(0, index);
-//       supportNameSpace
-//         .to("supportRoom")
-//         .emit("support_disconnect", remianSupports);
-//     });
-//   }
+    socket.on("support_msg", (msg, room, callback) => {
+      // new client message notif to supports
+      supportNameSpace.to(room).emit("new_msg", { msg, room });
+      callback();
+    });
+    //get client first msg
+    socket.on("loaded", room => {
+      // socket.join(room);
+      const stringRoom = JSON.stringify(room);
 
-//   //client room
-//   socket.on("client_join", room => {
-//     console.log("client joined");
-//     //client chat
-//     socket.join(room);
+      RedisClient.hgetall(stringRoom, function (err, value) {
+        if (value && value.msg)
+          supportNameSpace.to(room).emit("client_first_msg_server", value.msg);
+      });
+    });
 
-//     // RedisClient.hgetall(JSON.stringify(room), function (err, value) {
-//     //   if (value && value.msg) {
-//     //     socket.emit("send_saved_messages", value.messages, room);
-//     //   }
-//     // });
+    //on disconnect
+    socket.on("disconnect", () => {
+      const index = connectedSupports.findIndex(
+        support => support._id === socket.user._id,
+      );
+      const remianSupports = connectedSupports.splice(0, index);
+      supportNameSpace
+        .to("supportRoom")
+        .emit("support_disconnect", remianSupports);
+    });
+  }
 
-//     socket.on("clinet_msg", (msg, room, callback) => {
-//       // new client message notif to supports
-//       supportNameSpace
-//         .to("supportRoom")
-//         .emit("new_msg_notif", { sent: true, room });
-//       supportNameSpace.to(room).emit("new_msg", { msg, room });
-//       callback();
-//     });
+  //client room
+  socket.on("client_join", room => {
+    console.log("client joined");
+    //client chat
+    socket.join(room);
 
-//     socket.on("client_first_msg", (msg, room) => {
-//       const stringRoom = JSON.stringify(room);
-//       RedisClient.hset(stringRoom, "msg", JSON.stringify(msg));
-//     });
+    // RedisClient.hgetall(JSON.stringify(room), function (err, value) {
+    //   if (value && value.msg) {
+    //     socket.emit("send_saved_messages", value.messages, room);
+    //   }
+    // });
 
-//     //   let messages = [];
-//     //   socket.on("client_support_saved_messages", msg => {
-//     //     messages.push(msg);
-//     //     const stringMessages = JSON.stringify(messages);
-//     //     RedisClient.hset(JSON.stringify(room), "messages", stringMessages);
-//     //   });
-//   });
+    socket.on("clinet_msg", (msg, room, callback) => {
+      // new client message notif to supports
+      supportNameSpace
+        .to("supportRoom")
+        .emit("new_msg_notif", { sent: true, room });
+      supportNameSpace.to(room).emit("new_msg", { msg, room });
+      callback();
+    });
 
-//   uploader.on("saved", function (event) {
-//     // socket.on("file_co")
-//     const room = event.file.meta.roomId;
-//     supportNameSpace.to(room).emit("file_complete", {
-//       message: "",
-//       from: "client",
-//       file: event.file.name,
-//     });
-//   });
+    socket.on("client_first_msg", (msg, room) => {
+      const stringRoom = JSON.stringify(room);
+      RedisClient.hset(stringRoom, "msg", JSON.stringify(msg));
+    });
 
-//   socket.on("leave_room", room => {
-//     socket.leave(room);
-//   });
+    //   let messages = [];
+    //   socket.on("client_support_saved_messages", msg => {
+    //     messages.push(msg);
+    //     const stringMessages = JSON.stringify(messages);
+    //     RedisClient.hset(JSON.stringify(room), "messages", stringMessages);
+    //   });
+  });
 
-//   socket.on("disconnect", () => {});
-// });
+  uploader.on("saved", function (event) {
+    // socket.on("file_co")
+    const room = event.file.meta.roomId;
+    supportNameSpace.to(room).emit("file_complete", {
+      message: "",
+      from: "client",
+      file: event.file.name,
+    });
+  });
+
+  socket.on("leave_room", room => {
+    socket.leave(room);
+  });
+
+  socket.on("disconnect", () => {});
+});
 
 process.on("unhandledRejection", err => {
   console.log("UNHANDLED REJECTION! 💥 Shutting down...");
