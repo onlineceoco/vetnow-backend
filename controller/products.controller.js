8859aa2494ba6d7ff6c48b56a90e1e43d248468e
vicfw <faridforward2223@gmail.com>
2021-10-16 11:43:18 +0330

    fixig a error text

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -11,7 +11,7 @@ const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image")) {
     cb(null, true);
   } else {
-    cb(new AppError("Not an image! Please upload only images.", 400), false);
+    cb(new AppError("لطفا یک عکس آپلود کنید.", 400), false);
   }
 };
 
