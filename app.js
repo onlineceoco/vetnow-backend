200aef6f9e08f547540de60ee99aaf0d0f5cf0b4
vicfw <faridforward2223@gmail.com>
2021-10-16 16:17:11 +0330

    fixing image upload

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 // app.use("/api", limiter);
 app.use(helmet());
 
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json());
 
 //no-sql injection after body parser
 app.use(mongoSanitize());
